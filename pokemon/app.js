6baed2882b363c84e65f08d4d63edaa84f631eac
Abbe <abdullah.abou.shihab@icloud.com>
2023-02-27 15:05:10 +0100

    update

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,81 +1,97 @@
-let pokemons = [];
-const poke_container = document.getElementById("poke_container");
-const url = "https://pokeapi.co/api/v2/pokemon";
-const pokemons_number = 300;
-const search = document.getElementById("search");
-const form = document.getElementById("form");
+// Hämta element från DOM
+const searchInput = document.getElementById('search-pokemon');
+const pokemonList = document.getElementById('pokemon-ul');
+const teamList = document.getElementById('team-ul');
+const teamMessage = document.getElementById('team-message');
+const addButton = document.getElementById('add-button');
+const clearButton = document.getElementById('clear-button');
 
-const fetchPokemons = async () => {
-    for (let i = 1; i <= pokemons_number; i++) {
-        await getAllPokemon(i);
-    }
-    pokemons.forEach((pokemon) => createPokemonCard(pokemon));
-};
+// Skapa variabler för att spåra det nuvarande laget
+let team = [];
 
-const removePokemon = () => {
-    const pokemonEls = document.getElementsByClassName("pokemon");
-    let removablePokemons = [];
-    for (let i = 0; i < pokemonEls.length; i++) {
-        const pokemonEl = pokemonEls[i];
-        removablePokemons = [...removablePokemons, pokemonEl];
-    }
-    removablePokemons.forEach((remPoke) => remPoke.remove());
-};
-const getPokemon = async (id) => {
-    const searchPokemons = pokemons.filter((poke) => poke.name === id);
-    removePokemon();
-    searchPokemons.forEach((pokemon) => createPokemonCard(pokemon));
-};
-const getAllPokemon = async (id) => {
-    const res = await fetch(`${url}/${id}`);
-    const pokemon = await res.json();
-    pokemons = [...pokemons,pokemon];
-};
-fetchPokemons();
-
-function createPokemonCard(pokemon) {
-    const pokemonEl = document.createElement("div");
-    pokemonEl.classList.add("pokemon");
-    const poke_types = pokemon.types.map((el) => el.type.name).slice(0, 1);
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-    const poke_stat = pokemon.stats.map((el) => el.stat.name);
-    const stats = poke_stat.slice(0, 3);
-    const base_value = pokemon.stats.map((el) => el.base_stat);
-    const base_stat = base_value.slice(0, 3);
-    const stat = stats.map((stat) => {
-        return `<li class="names">${stat}</li>`;
-    }).join("");
-    const base = base_stat.map((base) => {
-        return `<li class="base">${base}</li>`
-    }).join("");
-    const pokeInnerHTML = `<div class="img-container">
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1${pokemon.id}.png" alt="${name}"/>
-    </div>
-    <div class="info">
-    <span class="number">#${pokemon.id.toString().padStart(3, "0")}</span>
-    <h3 class="name">${name}</h3>
-    <small class="type"><span>${poke_types}</span></small>
-    </div>
-    <div class="stats">
-    <h2>Stats</h2>
-    <div class="flex">
-    <ul>${stat}</ul>
-    <ul>${base}</ul>
-    </div>
-    </div>`;
-    pokemonEl.innerHTML = pokeInnerHTML;
-    poke_container.appendChild(pokemonEl);
+// Funktion för att hämta data från API:et
+async function fetchPokemonData(name) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const searchTerm = search.value;
-    if (searchTerm) {
-        getPokemon(searchTerm);
-        search.value = "";
-    } else if (searchTerm === "") {
-        pokemons = [];
-        removePokemon();
-        fetchPokemons();
-    }
-});
+// Funktion för att visa sökresultat
+async function displayPokemonList(pokemonArray) {
+  pokemonList.innerHTML = '';
+  pokemonArray.forEach (async pokemon => {
+    let image = await getImage(pokemon.url);
+    console.log(image);
+    const li = document.createElement('li');
+    let img = document.createElement('img')
+    img.src=image
+    li.appendChild(img)
+    li.innerHTML += pokemon.name;
+    li.addEventListener('click', () => addPokemonToTeam(pokemon));
+    pokemonList.appendChild(li);
+  });
+}
+async function getImage(url){
+    let response = await fetch(url)
+    let data = await response.json()
+    return await data.sprites.front_default
+}
+// Funktion för att söka efter Pokémons
+async function searchPokemon() {
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm.length === 0) {
+    return;
+  }
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1000`);
+  const data = await response.json();
+  const pokemonArray = data.results.filter(pokemon => {
+    return pokemon.name.includes(searchTerm.toLowerCase());
+  });
+  displayPokemonList(pokemonArray);
+}
+
+// Funktion för att lägga till en Pokémon till laget
+function addPokemonToTeam(pokemon) {
+  if (team.length >= 3) {
+    teamMessage.textContent = 'Du har redan tre ordinarie medlemmar i ditt lag.';
+    return;
+  }
+  team.push(pokemon);
+  teamMessage.textContent = '';
+  displayTeam();
+}
+
+// Funktion för att ta bort en Pokémon från laget
+function removePokemonFromTeam(index) {
+  team.splice(index, 1);
+  displayTeam();
+}
+
+// Funktion för att visa laget
+function displayTeam() {
+  teamList.innerHTML = '';
+  team.forEach((pokemon, index) => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = pokemon.sprites.front_default;
+    const button = document.createElement('button');
+    button.textContent = 'Ta bort';
+    button.addEventListener('click', () => removePokemonFromTeam(index));
+    li.appendChild(img);
+    li.appendChild(button);
+    teamList.appendChild(li);
+  });
+}
+
+// Funktion för att rensa hela laget
+function clearTeam() {
+  team = [];
+  displayTeam();
+}
+
+// Lyssna efter händelser
+searchInput.addEventListener('input', searchPokemon);
+addButton.addEventListener('click', () => searchPokemon());
+clearButton.addEventListener('click', clearTeam);
+
